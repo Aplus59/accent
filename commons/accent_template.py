9a8542003a25f267d8de00f5b7acd2c2a348f3c2
Nguyễn Hồ Bảo Khánh <94693152+Aplus59@users.noreply.github.com>
2025-02-17 18:59:29 +0700

    Update accent_template.py

--- commons/accent_template.py
+++ commons/accent_template.py
@@ -3,43 +3,36 @@ import math
 
 from commons.explanation_algorithm_template import ExplanationAlgorithmTemplate
 from commons.handle_causal import find_causal,find_child
-def min_items_greater_than_a(sum_infl, a):
+def find_counterfactual_set(sum_infl, a):
     # Step 1: Create a dictionary to group items by weight (length of list)
     all_items = {}
     for list_item, value in sum_infl:
         weight = len(list_item)
         if weight not in all_items:
             all_items[weight] = []
-        all_items[weight].append({'item': list_item, 'value': value})
+        if value > 0:
+            all_items[weight].append({'item': list_item, 'value': value})
 
     # Sort each group by descending value
     for weight in all_items:
         all_items[weight].sort(key=lambda x: x['value'], reverse=True)
 
-    print("w", all_items)
 
     
-    list_item = [{'item': None,'value': 0,'taken_index': [0] * len(sum_infl)}]  # Stores the highest odd-summed subset
+    list_item = [[{'item': [],'value': 0,'taken_index': [0] * len(sum_infl)}]]  # Stores the highest odd-summed subset
     taken_index = [0] * len(sum_infl)
     taken_index[1] = 1
     list_item.append([{'item': all_items[1][0]['item'],'value': all_items[1][0]['value'],'taken_index': taken_index}])  # Stores the highest odd-summed subset
     
-    if all_items[1][0]['value'] >= a:
+    if all_items[1][0]['value'] > a:
         return  all_items[1][0]['item'], a - all_items[1][0]['value']
-    print("list_item", list_item[0])
     
     for i in range(2, len(sum_infl) + 1):
         total = [{'value': 0, 'item': [],'taken_index':[0] * (len(sum_infl) + 2)} for _ in range(i *2)]
-        if i in all_items and 1 in all_items:
-            total[0]['value'] = all_items[i][0]['value']
-            total[0]['item'] = all_items[i][0]['item']
-            total[0]['taken_index'][i] += 1
-        print("index",i)
-        for j in range(1,i+1):
-            print("go go", i - j, j)
+        for j in range(0,i):
             if j  < len(list_item) and len(list_item[j]) > 0:
                 taken_idx = list_item[j][0]['taken_index']
-                if i - j in all_items and taken_idx[i - j] < len(all_items[i - j]) :
+                if i - j in all_items and taken_idx[i - j] < len(all_items[i - j]):
                     keys = all_items[i - j][taken_idx[i - j]]['item']
                     overlap_items = set(keys) & set(list_item[j][0]['item'])
                     if not overlap_items:
@@ -51,18 +44,14 @@ def min_items_greater_than_a(sum_infl, a):
                         )
                         total[j]['taken_index'] = taken_idx[:]
                         total[j]['taken_index'][i - j] += 1
-                        print("not_overlap_list_item", list_item[j][0]['item'], "all", all_items[i - j][taken_idx[i - j]]['item'])
 
                     else:
-
-                        print("ovl")
                         index = 0
                         while (
                             i - j in all_items and taken_idx[i - j] + index < len(all_items[i - j]) and j < len(list_item)
                             and set(all_items[i - j][taken_idx[i - j] + index]['item']) & set(list_item[j][0]['item'])
                         ):
                             index += 1
-                        print("index",index)
                         if i - j in all_items and taken_idx[i - j] + index < len(all_items[i - j]) and j < len(list_item):
                             total[j]['value'] = (
                                 list_item[j][0]['value'] + all_items[i - j][taken_idx[i - j] + index]['value']
@@ -72,7 +61,6 @@ def min_items_greater_than_a(sum_infl, a):
                             )
                             total[j]['taken_index'] = taken_idx[:]
                             total[j]['taken_index'][i - j] += index
-                            print("overlap_list_item", list_item[j][0]['item'], "all", all_items[i - j][taken_idx[i - j] + index]['item'])
                         m = 1
                         
                         while (
@@ -80,9 +68,7 @@ def min_items_greater_than_a(sum_infl, a):
                             and set(all_items[i - j][taken_idx[i - j]]['item']) & set(list_item[j][m]['item'])
                         ):
                             m+=1
-                        print("m",m)
                         if i - j in all_items and taken_idx[i - j] < len(all_items[i - j]) and j < len(list_item) and m < len(list_item[j]):
-                            print("item",list_item[j][m])
                             total[j + i]['value'] = (
                                 list_item[j][m]['value'] + all_items[i - j][taken_idx[i - j]]['value']
                             )
@@ -91,10 +77,9 @@ def min_items_greater_than_a(sum_infl, a):
                             )
                             total[j + i]['taken_index'] = taken_idx[:]
                             total[j + i]['taken_index'][i - j] += 1
-                            print("overlap_list_item_2", list_item[j][m]['item'], "all", all_items[i - j][taken_idx[i - j]]['item'])
                             
 
-        filtered_totals = [(index, t) for index, t in enumerate(total) if t['value'] >= a]
+        filtered_totals = [(index, t) for index, t in enumerate(total) if t['value'] > a]
         if filtered_totals:
             min_index = min(filtered_totals, key=lambda x: x[1]['value'])[0]
             return total[min_index]['item'], a - total[min_index]['value']
@@ -104,10 +89,7 @@ def min_items_greater_than_a(sum_infl, a):
             key=lambda x: x['value'], 
             reverse=True  # Sắp xếp giảm dần
         )
-        print("all", sorted_total)
-        
         list_item.append(sorted_total)
-
     return [], 0  
 class AccentTemplate(ExplanationAlgorithmTemplate):
     @staticmethod
@@ -125,7 +107,7 @@ class AccentTemplate(ExplanationAlgorithmTemplate):
         """
         causal_tree = find_causal()
         print(f'try replace', repl, score_gap)
-
+        print("visited",visited)
         causal_list = []
         for id in visited:
             if id == 0:
@@ -146,18 +128,18 @@ class AccentTemplate(ExplanationAlgorithmTemplate):
                 index_causal_list.append(visited.index(item))
 
         # Tính toán ảnh hưởng với cấu trúc nhân quả
-        sorted_infl = []
+        sum_infl = []
         for idx_group in index_causal_list:
             if isinstance(idx_group, list):
                 # Tổng ảnh hưởng trong nhóm nhân quả
                 group_infl = sum(gap_infl[idx] for idx in idx_group)
-                sorted_infl.append((idx_group, group_infl))
+                sum_infl.append((idx_group, group_infl))
             else:
-                sorted_infl.append(([idx_group], gap_infl[idx_group]))
+                sum_infl.append(([idx_group], gap_infl[idx_group]))
 
-        print("list", sorted_infl,"sg", score_gap)
+        print("list", sum_infl,"sg", score_gap)
         # Sắp xếp ưu tiên số lượng phần tử trong tuple nhỏ nhất, nếu số lượng phần tử bằng nhau, xét đến infl
-        removed_items, score_gap = min_items_greater_than_a(sorted_infl, score_gap)
+        removed_items, score_gap = find_counterfactual_set(sum_infl, score_gap)
         print("Score gap",score_gap)
         if score_gap < 0:
             print(f'replace {repl}: {removed_items}')
