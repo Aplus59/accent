084bb93c2444873d782d2965a642d6fbf8a66978
khanh <baokhanhnguyenho@gmail.com>
2026-02-09 12:37:58 +0100

    fix bug

--- RCF/src/experiment.py
+++ RCF/src/experiment.py
@@ -14,7 +14,6 @@ def main():
     run the full experiment for an algorithm passed via the command line argument --algo
     """
     ks = [20]
-    generate_cf(ks)
     retrain(ks)
     get_new_scores(ks)
     evaluate_files(parse_args, ks)
